a78e3c09f796de975a3e4ca564e357fba3440120
vpreseault <vincent.preseault@gmail.com>
2025-03-26 15:47:49 -0400

    update activity display queries

--- backend/internal/queries/main.go
+++ backend/internal/queries/main.go
@@ -1,33 +1,6 @@
 package queries
 
-// Auth
-var CreateCustomer = `
-INSERT INTO Customers (
-	full_name,
-	ID_type, 
-	ID_number,
-	address,
-	registration_date
-) VALUES ($1, $2, $3, $4, $5)
-RETURNING customer_ID
-`
-
-var GetCustomerByName = `SELECT * FROM Customers WHERE full_name = $1`
-
-var CreateEmployee = `
-INSERT INTO Employees (
-	hotel_ID,
-	full_name, 
-	address, 
-	ID_type, 
-	ID_number,
-	role
-) VALUES ($1, $2, $3, $4, $5, $6)
-RETURNING employee_ID
-`
-
-var GetEmployeeByName = `SELECT * FROM Employees WHERE full_name = $1`
-
+// Search
 var BaseRoomSearch = `SELECT 
 	room_ID,
 	hotel_ID,
@@ -42,10 +15,5 @@ var BaseRoomSearch = `SELECT
 	address,
 	total_rooms
 FROM RoomSearchView`
-
-// Activity
-var GetCustomerBookings = `SELECT * FROM Bookings WHERE customer_id = $1`
-var GetCustomerRentings = `SELECT * FROM Rentings WHERE customer_id = $1`
-var GetCustomerArchives = `SELECT * FROM Archives WHERE customer_id = $1`
 
 var GetEmployeeHotelID = `SELECT hotel_ID FROM Employees WHERE employee_ID = $1`
