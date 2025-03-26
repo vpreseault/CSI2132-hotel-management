1773397f697648ca561da8db63d1d2d56633c2b7
Nicolas_M <nsoccer344@outlook.com>
2025-03-26 15:49:03 -0400

    Fixed all the comments left on the PR

--- frontend/src/components/LandingPage/Booking.vue
+++ frontend/src/components/LandingPage/Booking.vue
@@ -6,7 +6,7 @@
 
     <template v-if="!isEmployee">
       <div class="text-center my-4">
-        <button @click="isSearchCardVisible = !isSearchCardVisible" class="px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600 transition">
+        <button @click="isSearchCardVisible = !isSearchCardVisible" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-indigo-600 transition">
           {{ isSearchCardVisible ? 'Close' : 'Search' }}
         </button>
       </div>
