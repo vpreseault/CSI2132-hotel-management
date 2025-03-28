08374ac47f74edc477516d833ed4ddcb1fa727f4
vpreseault <vincent.preseault@gmail.com>
2025-03-28 13:18:13 -0400

    display hotel rentals

--- frontend/src/pages/EmployeeDashboard.vue
+++ frontend/src/pages/EmployeeDashboard.vue
@@ -6,7 +6,26 @@
       </div>
   
       <NavBar role="employee" @toggleProfile="toggleProfileModal" @toggleHotel="toggleHotelModal" />
-      <Rental :expandedCard="expandedCard" :toggleCard="toggleCard" :isEmployee="true" :employeeRentals="allHotelRentings" />
+      
+      <LayoutSection title="All Hotel Rentings">
+        <div :class="gridColumns" class="mt-4 grid grid-cols-1 gap-4 justify-center">
+          <ActivityCard
+            v-for="(rental, index) in hotelRentals"
+            :key="`rental-${index}`"
+            :customerName="rental.customer_name"
+            :startDate="new Date(rental.start_date)"
+            :endDate="new Date(rental.end_date)"
+            :employeeName="rental.employee_name"
+            :roomNumber="rental.room_number"
+            :price="rental.total_price"
+            :payment="rental.payment"
+            :section="'rental'"
+            :index="index"
+            :expandedCard="expandedCard"
+            @toggle="toggleCard"
+          />
+        </div>
+      </LayoutSection>
       <Booking :expandedCard="expandedCard" :toggleCard="toggleCard" :isEmployee="true" @createBooking="handleEmployeeBooking" />
       <Profile v-if="isProfileModalOpen" role="employee" :toggleProfileModal="toggleProfileModal" />
       <HotelModal v-if="isHotelModalOpen" @close="toggleHotelModal" />
@@ -18,13 +37,13 @@
 </template>
   
 <script setup lang="ts">
-import { ref } from 'vue';
-import { removeAuthCookie } from '../utils/auth';
+import { computed, onMounted, ref } from 'vue';
+import { getUserID, removeAuthCookie } from '../utils/auth';
 import NavBar from '../components/LandingPage/NavBar.vue';
-import Rental from '../components/LandingPage/Rental.vue';
 import Booking from '../components/LandingPage/Booking.vue';
 import Profile from '../components/LandingPage/Profile.vue';
 import HotelModal from '../components/LandingPage/HotelModal.vue';
+import LayoutSection from '../components/Layout/LayoutSection.vue';
 
 type RentalItem = {
   cardType: 'booking' | 'renting' | 'archive';
@@ -66,50 +85,39 @@ function handleEmployeeBooking(booking: {
   checkoutDate: string;
   email: string;
 }) {
-  allHotelRentings.value.push({
-    customer_name: booking.customerName,
-    room_number: booking.roomNumber,
-    start_date: new Date().toISOString().split('T')[0],
-    end_date: booking.checkoutDate,
-    employee_name: "Current Employee",
-    cardType: "renting",
-    total_price: 0,
-    payment: false
-  });
+  // allHotelRentings.value.push({
+  //   customer_name: booking.customerName,
+  //   room_number: booking.roomNumber,
+  //   start_date: new Date().toISOString().split('T')[0],
+  //   end_date: booking.checkoutDate,
+  //   employee_name: "Current Employee",
+  //   cardType: "renting",
+  //   total_price: 0,
+  //   payment: false
+  // });
 }
 
-const allHotelRentings = ref<RentalItem[]>([
-  {
-    customer_name: "John Doe",
-    room_number: 101,
-    start_date: "2025-03-20",
-    end_date: "2025-03-25",
-    employee_name: "Employee A",
-    cardType: "renting",
-    total_price: 500,
-    payment: true
-  },
-  {
-    customer_name: "Jane Smith",
-    room_number: 202,
-    start_date: "2025-03-27",
-    end_date: "2025-03-30",
-    employee_name: "Employee A",
-    cardType: "renting",
-    total_price: 400,
-    payment: false
-  },
-  {
-    customer_name: "Bob Builder",
-    room_number: 303,
-    start_date: "2025-03-28",
-    end_date: "2025-04-02",
-    employee_name: "Employee A",
-    cardType: "renting",
-    total_price: 450,
-    payment: true
+const hotelRentals = ref<Array<RentalItem>>([])
+onMounted(async () => {
+  const employeeID = getUserID()
+  
+  try {
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_HOST}/api/rentings?employee_ID=${employeeID}`)
+      if (res.ok) {
+        // TODO: use data with RentalCard
+        hotelRentals.value = await res.json()
+      }
+  } catch (error) {
+      console.error('Error calling API:', error);
   }
-]);
+})
+
+const gridColumns = computed(() => {
+  if (hotelRentals.value.length > 1){
+    return `md:grid-cols-2 lg:grid-cols-2`
+  }
+  return `md:grid-cols-1 lg:grid-cols-1`
+})
 </script>
 
   
