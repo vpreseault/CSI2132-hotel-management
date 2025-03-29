74042c94946b5a4c87b01f2e1444af2e2f7be31a
vpreseault <vincent.preseault@gmail.com>
2025-03-28 21:07:51 -0400

    finish filter input

--- frontend/src/components/LandingPage/SearchSection.vue
+++ frontend/src/components/LandingPage/SearchSection.vue
@@ -1,96 +1,90 @@
 <template>
   <div>
-    <!-- <Card class="w-full max-w-3xl mx-auto mt-6"> -->
-      <!-- <template #content> -->
-        <div class="flex flex-wrap gap-6 mb-3">
-          <!-- Dates -->
-          <div class="flex flex-col">
-            <label class="font-medium">Dates</label>
-            <DatePicker v-model="filters.dates" selectionMode="range" :manualInput="false" showIcon fluid iconDisplay="input" placeholder="Select Dates" />
-          </div>
-          
-          <!-- City -->
-          <div class="flex flex-col">
-            <label class="font-medium">City</label>
-            <InputText v-model="filters.city" placeholder="Enter City Name" class="w-full" />
-          </div>
+    <div class="flex flex-wrap justify-center gap-6 mb-5">
+      <!-- Dates -->
+      <div class="flex flex-col">
+        <label class="font-medium">Dates</label>
+        <DatePicker v-model="filters.dates" selectionMode="range" :manualInput="false" showIcon fluid iconDisplay="input" placeholder="Select Dates" />
+        <Message v-if="error" severity="error" size="small" variant="simple">{{ error }}</Message>
+      </div>
+      
+      <!-- City -->
+      <div class="flex flex-col">
+        <label class="font-medium">City</label>
+        <InputText v-model="filters.city" placeholder="Enter City Name" class="w-full" />
+      </div>
 
-          <!-- Hotel Chain -->
-          <div class="flex flex-col">
-            <label class="font-medium">Hotel Chain</label>
-            <Dropdown v-model="filters.chain" :options="chains" optionLabel="chain_name" optionValue="chain_ID" placeholder="Select Hotel Chain" class="w-full"/>
-          </div>
-        </div>
+      <!-- Hotel Chain -->
+      <div class="flex flex-col">
+        <label class="font-medium">Hotel Chain</label>
+        <Select v-model="filters.chain" :options="chains" optionLabel="chain_name" optionValue="chain_ID" placeholder="Select Hotel Chain" class="w-full"/>
+      </div>
+    </div>
 
-          <!-- Sliders -->
-        <div class="flex flex-wrap gap-6 mb-3">
-          <div class="flex items-center gap-4">
-            <div class="flex-1">
-              <label class="font-medium">Room Capacity: {{ filters.roomCapacity }}</label>
-              <Slider v-model="filters.roomCapacity" :min="1" :max="10" class="w-56 mt-3" />
-            </div>
-          </div>
-          <div class="flex items-center gap-4">
-            <div class="flex-1">
-              <label class="font-medium">Hotel Category: {{ filters.hotelCategory }}</label>
-              <Slider v-model="filters.hotelCategory" :min="1" :max="5" class="w-56 mt-3" />
-            </div>
-          </div>
-          <div class="flex items-center gap-4">
-            <div class="flex-1">
-              <label class="font-medium">Total Hotel Rooms: {{ filters.totalRooms }}</label>
-              <Slider v-model="filters.totalRooms" :min="1" :max="25" class="w-56 mt-3" />
-            </div>
-          </div>
-          <div class="flex items-center gap-4">
-            <div class="flex-1">
-              <label class="font-medium">Max Room Price: ${{ filters.roomPrice }}</label>
-              <Slider v-model="filters.roomPrice" :min="50" :max="1000" class="w-56 mt-3" />
-            </div>
-          </div>
+  <!-- Sliders -->
+    <div class="flex flex-wrap justify-center gap-6 mb-10">
+      <div class="flex items-center gap-4">
+        <div class="flex-1">
+          <label class="font-medium">Room Capacity: {{ filters.roomCapacity }}</label>
+          <Slider v-model="filters.roomCapacity" :min="1" :max="10" class="w-56 mt-3" />
         </div>
-        <!-- Search Button -->
-        <div class="flex justify-center mb-3">
-          <Button label="Search" icon="pi pi-search" @click="submitSearch" />
+      </div>
+      <div class="flex items-center gap-4">
+        <div class="flex-1">
+          <label class="font-medium">Hotel Category: {{ filters.hotelCategory }}</label>
+          <Slider v-model="filters.hotelCategory" :min="1" :max="5" class="w-56 mt-3" />
         </div>
-      <!-- </template> -->
-    <!-- </Card> -->
-
-    <SearchCard :rooms="searchResults" :show="showDialog" @close="showDialog = false" />
+      </div>
+      <div class="flex items-center gap-4">
+        <div class="flex-1">
+          <label class="font-medium">Total Hotel Rooms: {{ filters.totalRooms }}</label>
+          <Slider v-model="filters.totalRooms" :min="1" :max="25" class="w-56 mt-3" />
+        </div>
+      </div>
+      <div class="flex items-center gap-4">
+        <div class="flex-1">
+          <label class="font-medium">Max Room Price: {{filters.roomPrice ? `$${filters.roomPrice}`:'' }}</label>
+          <Slider v-model="filters.roomPrice" :min="50" :max="1000" class="w-56 mt-3" />
+        </div>
+      </div>
+    </div>
+    <!-- Search Button -->
+    <div class="flex justify-center gap-6">
+      <Button label="Search" icon="pi pi-search" @click="submitSearch" />
+      <Button label="Clear Filters"severity="secondary" icon="pi pi-times" @click="clearFilters" />
+    </div>
+    {{ searchResults }}
+    <!-- <SearchCard :rooms="searchResults" :show="showDialog" @close="showDialog = false" /> -->
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onMounted } from 'vue';
-import Dropdown from 'primevue/dropdown';
+import { ref, onMounted, reactive } from 'vue';
 import Slider from 'primevue/slider';
 import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
-import SearchCard from './SearchCard.vue';
 
-interface Room {
-  hotel_name: string;
-  room_number: number;
-  capacity: number;
-  price: number;
-  view_type: string;
-  extendable: boolean;
-  damaged: boolean;
-  chain_name: string;
-  category: number;
-  address: string;
-  total_rooms: number;
-}
-
-const filters = ref({
-  city: '',
+const filters = reactive<{
+  dates: Date[],
+  city?: string,
+  chain?: string,
+  roomCapacity?: number,
+  hotelCategory?: number,
+  totalRooms?: number,
+  roomPrice?: number,
+}>({
   dates: [],
-  chain: '',
-  roomCapacity: 1,
-  hotelCategory: 1,
-  totalRooms: 5,
-  roomPrice: 200
 });
+
+function clearFilters() {
+  filters.dates = []
+  filters.city = undefined
+  filters.chain = undefined
+  filters.roomCapacity = undefined
+  filters.hotelCategory = undefined
+  filters.totalRooms = undefined
+  filters.roomPrice = undefined
+}
 
 type Chain = {
   chain_ID: number
@@ -103,7 +97,6 @@ onMounted(async () => {
   try {
       const res = await fetch(`${import.meta.env.VITE_BACKEND_HOST}/api/chains`)
       if (res.ok) {
-        // TODO: use data with RentalCard
         chains.value = await res.json()
       }
   } catch (error) {
@@ -111,55 +104,66 @@ onMounted(async () => {
   }
 })
 
-const searchResults = ref<Room[]>([]);
+const searchResults = ref<SearchResult[]>([]);
 const showDialog = ref(false);
 
-function submitSearch() {
-  const { startDate, endDate } = filters.value;
+type SearchResult = {
+  room_ID: number,
+	hotel_ID: number,
+	room_number: string,
+	capacity: number,
+	price: number,
+	view_type: string,
+	extendable: boolean,
+	damaged: boolean,
+	chain_name: string,
+	category: number,
+	address: string,
+	total_rooms: number,
+}
 
-  if (!startDate || !endDate) {
-    alert('Please select both start and end dates.');
-    return;
+const error = ref('')
+function validate() {
+  error.value = ''
+  if (!filters?.dates || !filters.dates[0] || !filters.dates[1]) {
+    error.value  = 'Dates are required.'
+    return false
+  } else {
+    const oneDay = 24 * 60 * 60 * 1000;
+    const duration = (filters.dates[1].getTime() - filters.dates[0].getTime()) / oneDay;
+    if (duration < 1) {
+      error.value = 'Booking duration must be at least 1 night.'
+      return false
+    } 
   }
+  return true
+}
 
-  const oneDay = 24 * 60 * 60 * 1000;
-  const duration = (endDate.getTime() - startDate.getTime()) / oneDay;
-
-  if (duration < 1) {
-    alert('Bookings must be at least 2 days long.');
-    return;
-  }
-
-  searchResults.value = [
-    {
-      hotel_name: 'Marriott',
-      room_number: 101,
-      capacity: 3,
-      price: 250,
-      view_type: 'Sea',
-      extendable: true,
-      damaged: false,
-      chain_name: 'Marriott Group',
-      category: 4,
-      address: '123 Ocean View Blvd, Miami, FL',
-      total_rooms: 200
-    },
-    {
-      hotel_name: 'Holiday Inn',
-      room_number: 202,
-      capacity: 2,
-      price: 180,
-      view_type: 'City',
-      extendable: false,
-      damaged: false,
-      chain_name: 'IHG',
-      category: 3,
-      address: '789 City Center Ave, Orlando, FL',
-      total_rooms: 150
+async function submitSearch() {
+    if (validate()) {
+        try {
+          const res = await fetch(`${import.meta.env.VITE_BACKEND_HOST}/api/search`,
+              {
+                  method: 'POST',
+                  body: JSON.stringify({
+                    "start_date": filters.dates[0],
+                    "end_date": filters.dates[1],
+                    "capacity": filters.roomCapacity,
+                    "chain_name": filters.chain,
+                    "area": filters.city,
+                    "category": filters.hotelCategory,
+                    "max_price": filters.roomPrice,
+                    "total_rooms": filters.totalRooms,
+                  })
+              }
+          )
+          if (res.ok) {
+              searchResults.value = await res.json()
+          }
+        } catch (error) {
+            console.error('Error calling API:', error);
+        }
     }
-  ];
-
-  showDialog.value = true;
 }
 </script>
 
