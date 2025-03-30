8fdd9d56dd7f9849273af950bb11c5104772372b
vpreseault <vincent.preseault@gmail.com>
2025-03-30 09:03:21 -0400

    Implement modal

--- frontend/src/components/LandingPage/NavBar.vue
+++ frontend/src/components/LandingPage/NavBar.vue
@@ -4,8 +4,8 @@
       <li v-if="role === 'customer'" :class="navItemClass"> Book </li>
       <li v-if="role === 'customer'" :class="navItemClass"> Rentings </li>
   
-      <li v-if="role === 'employee' || role === 'manager'" @click="emit('toggleHotel')" :class="navItemClass"> Hotel </li>
-      <li v-if="role === 'manager'" @click="emit('toggleCreateEmployee')" :class="navItemClass" > Create Employee </li>
+      <li v-if="role === 'manager'" @click="emit('toggleHotel')" :class="navItemClass">Edit Hotel</li>
+      <li v-if="role === 'manager'" @click="emit('toggleCreateEmployee')" :class="navItemClass" >Create Employee</li>
   
       <li @click="emit('toggleProfile')" :class="navItemClass"> Profile </li>
       <button class="ml-8 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition" @click="handleLogout">Logout</button>
