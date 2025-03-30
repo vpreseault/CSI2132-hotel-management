f7dd4a94da6049fa35e25a9f518b6eff4449d99b
Nicolas_M <nsoccer344@outlook.com>
2025-03-30 11:52:20 -0400

    Updated login page

--- frontend/src/components/Account/LoginUser.vue
+++ frontend/src/components/Account/LoginUser.vue
@@ -1,15 +1,18 @@
 <template>
-    <Form v-slot="$form" :resolver="resolver" @submit="onFormSubmit" class="flex flex-col gap-4 w-full sm:w-56">
-        <div class="flex flex-col gap-1">
+    <Form v-slot="$form" :resolver="resolver" @submit="onFormSubmit" class="flex flex-col gap-4 w-full sm:w-56 ">
+        <div class="flex flex-col gap-1 ">
             <InputText name="fullname" type="text" placeholder="Full name" fluid />
-            <Message v-if="$form.fullname?.invalid" severity="error" size="small" variant="simple">{{ $form.fullname.error?.message }}</Message>
+            <Message v-if="$form.fullname?.invalid" severity="error" size="small" variant="simple"> {{ $form.fullname.error?.message }} </Message>
+            <Message v-if="nameNotFound" severity="error" size="small" variant="simple"> Name not found in the database </Message>
         </div>
         <Button type="submit" severity="secondary" label="Submit" />
     </Form>
 </template>
+
 <script setup lang="ts">
-import { type FormResolverOptions, type FormSubmitEvent } from '@primevue/forms';
-import { setAuthCookie } from '../../utils/auth';
+import { ref } from 'vue'
+import { type FormResolverOptions, type FormSubmitEvent } from '@primevue/forms'
+import { setAuthCookie } from '../../utils/auth'
 
 const props = defineProps<{
     employee: boolean
@@ -20,38 +23,49 @@ interface Error {
     message: string;
 }
 
+const nameNotFound = ref(false)
+
 const resolver = ({ values }: FormResolverOptions) => {
     const errors: Error[] = []
 
     if (!values.fullname) {
-        errors.push({ type: 'required', message: 'Full name is required.' });
+        errors.push({ type: 'required', message: 'Full name is required.' })
     }
 
     return {
         values,
         errors
-    };
-};
+    }
+}
 
 async function onFormSubmit(e: FormSubmitEvent) {
+    nameNotFound.value = false  
+
     if (e.valid) {
         try {
-            const res = await fetch(`${import.meta.env.VITE_BACKEND_HOST}/api/${props.employee ? `employees?employee_name=${e.values.fullname}` : `customers/${e.values.fullname}`}`)
+            const url = props.employee
+                ? `${import.meta.env.VITE_BACKEND_HOST}/api/employees?employee_name=${e.values.fullname}`
+                : `${import.meta.env.VITE_BACKEND_HOST}/api/customers/${e.values.fullname}`
+
+            const res = await fetch(url)
+
             if (res.ok) {
                 const data = await res.json()
 
                 const user = {
-                    ID: props.employee ? data.employee_ID: data.customer_ID,
+                    ID: props.employee ? data.employee_ID : data.customer_ID,
                     name: data.full_name,
                     role: data?.role ? data.role : 'Customer',
                 }
 
                 setAuthCookie(user)
+            } else {
+                nameNotFound.value = true
             }
         } catch (error) {
-            console.error('Error calling API:', error);
+            console.error('Error calling API:', error)
+            nameNotFound.value = true
         }
     }
 }
 </script>
-    
