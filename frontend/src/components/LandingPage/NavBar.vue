468e75fe9297fcde20657b6d76bba9586f6e2d88
Nicolas_M <nsoccer344@outlook.com>
2025-03-31 10:18:21 -0400

    Minor fixes

--- frontend/src/components/LandingPage/NavBar.vue
+++ frontend/src/components/LandingPage/NavBar.vue
@@ -1,11 +1,8 @@
 <template>
-  <div class="bg-green-100 py-4 fixed top-0 left-0 w-full z-10 shadow-md">
+  <div class="bg-green-100 py-3 fixed top-0 left-0 w-full z-10 shadow-md">
     <div class="flex justify-between items-center mx-8">
       <h1 class="text-2xl font-bold text-black">Ω Hotel Management</h1>
       <ul class="flex items-center space-x-6">
-        <li v-if="role === 'customer'" :class="navItemClass"> Book </li>
-        <li v-if="role === 'customer'" :class="navItemClass"> Rentings </li>
-
         <li v-if="role === 'manager'" @click="emit('toggleHotel')" :class="navItemClass">Edit Hotel</li>
         <li v-if="role === 'manager'" @click="emit('toggleCreateEmployee')" :class="navItemClass">Create Employee</li>
 
